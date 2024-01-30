0b4484af291be0019fbfd817c48c8a7b7c568a04
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2024-01-30 11:35:24 -0700

    Update sphere math.py

--- sphere math.py
+++ sphere math.py
@@ -7,3 +7,7 @@ sphere_area = 4 * math.pi * (sphere_radius ** 2)
 
 
 print(f'{sphere_area:.2f}')
+
+#This program first reads the sphere radius as a float from the user, then calculates the
+#surface area using the formula, and finally prints the result rounded to two decimal 
+#places. If the input is 3.10, the output should be 120.76.
